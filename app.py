cc29d450e795d5855380911390ca83ff25eb9e37
Sinchana P R <sinchanapr23@gmail.com>
2026-05-26 15:32:21 +0530

    fixed port

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+
 # ── HOME PAGE (Bubble landing) ──────────────────────────────
 @app.route('/')
 def home():
@@ -272,4 +273,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
